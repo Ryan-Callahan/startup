ff06e1abbcd5fcb679bfae3ab05ae25fae78b01d
ryanc <ryan.callahan1124@gmail.com>
2025-03-18 18:56:09 -0600

    created class TimeUtils and created static methods from the methods previously in dashboard.jsx

---
+++ src/dashboard/TimeUtils.jsx
@@ -0,0 +1,49 @@
+class TimeUtils {
+    /**
+     * takes a time in epoch format and floors it to the current minute
+     * @param time
+     * @returns {number}
+     */
+    static getEpochToMinute(time) {
+        return (Math.floor(time / 60000) * 60000);
+    }
+
+    /**
+     * takes a time in epoch format and floors it to the current day
+     * @param time
+     * @returns {number}
+     */
+    static getEpochToDay(time) {
+        return (Math.floor(time / 86400000) * 86400000);
+    }
+
+    /**
+     * takes a Date, converts it to the current timezone time
+     * @param date
+     * @returns {Date}
+     */
+    static getTimezoneTime(date) {
+        return new Date(Math.floor(date.getTime()) - (date.getTimezoneOffset() * 60000));
+    }
+
+    /**
+     * returns the current time but on sunday of the current week
+     * @returns {Date}
+     */
+    static getCurrentWeek() {
+        let now = new Date();
+        now = new Date(self.getTimezoneTime(now));
+        now.setDate(now.getDate() - now.getDay())
+        return now;
+    }
+
+    /**
+     * tales a time in epoch format and converts it to a Date
+     * @param date
+     * @returns {Date}
+     */
+    static getDateFromEpoch(date) {
+        const then = new Date(date);
+        return (then);
+    }
+}
--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -4,38 +4,9 @@ import {Col, Container, Row} from "react-bootstrap";
 import {Calendar} from "./calendar";
 
 export function Dashboard() {
-    function getEpochToMinute(time) {
-        const roundToMinute = Math.floor(time / 60000) * 60000;
-        return (roundToMinute);
-    }
-
-    function getEpochToDay(time) {
-        const roundToHour = Math.floor(time / 86400000) * 86400000;
-        return (roundToHour);
-    }
-
-    //takes a time, converts it to the current timezone time
-    function getTimezoneTime(now) {
-        return new Date(Math.floor(now.getTime()) - (now.getTimezoneOffset() * 60000));
-    }
-
-    //returns the current time but on sunday of the current week to the minute
-    function getCurrentWeek() {
-        let now = new Date();
-        now = new Date(getTimezoneTime(now));
-        console.log(now.getTime());
-        now.setDate(now.getDate() - now.getDay())
-        return now.getTime();
-    }
-
-
-    function getDateFromEpoch(date) {
-        const then = new Date(date);
-        return (then);
-    }
 
     function getActiveWeek() {
-        return getDateFromEpoch(getEpochToMinute());
+        return TimeUtils.getCurrentWeek();
     }
 
     return (
@@ -51,7 +22,7 @@ export function Dashboard() {
                 <Row>
                     <Col>
                         <div style={{maxHeight: '80vh', overflow: 'auto', border: '3px solid black'}}>
-                            <Calendar />
+                            <Calendar activeWeek={getActiveWeek()} />
                         </div>
                     </Col>
                 </Row>
